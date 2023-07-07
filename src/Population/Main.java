a44a545e2f81bb96aebf015766629023be125aba
Danis Abdulgazizov <businessskyjoyboy@gmail.com>
2023-07-07 19:04:41 +0300

    Доработано

--- src/Population/Main.java
+++ src/Population/Main.java
@@ -28,10 +28,9 @@ public class Main {
                 .collect(Collectors.toList());
 
         List<Person> peopleWithHigherEducation = persons.stream()
-                .filter(x -> x.getAge() >= 18
-                        && ((x.getSex() == Sex.WOMAN && x.getAge() <= 60)
-                            || (x.getSex() == Sex.MAN && x.getAge() <=65))
-                                && x.getEducation() == Education.HIGHER)
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> (x.getSex() == Sex.WOMAN ? x.getAge() <= 60 : x.getAge() <= 65))
                 .sorted(Comparator.comparing(Person::getFamily))
                 .collect(Collectors.toList());
 
