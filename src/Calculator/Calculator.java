9a907f4e6933588fcf2d248de891b6cec510a25b
Danis Abdulgazizov <businessskyjoyboy@gmail.com>
2023-07-07 19:04:31 +0300

    Доработано

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -7,12 +7,7 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> {
-        if (y == 0) {
-            throw new ArithmeticException("Делитель равен 0");
-        }
-        return x / y;
-    };
+    BinaryOperator<Integer> divide = (x, y) -> y != 0 ? x / y : Integer.MAX_VALUE;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
@@ -21,12 +16,9 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator calculator = Calculator.instance.get();
         int a = calculator.plus.apply(1, 2);
-        int b = calculator.minus.apply(1, 1); // b == 0, поэтому выбрасывается исключение ArithmeticException: / by zero
-        try { // можем воспользоваться исключением или же проверкой b !=0 через if else
-            int c = calculator.divide.apply(a, b);
-            calculator.println.accept(c);
-        } catch (ArithmeticException e) {
-            System.out.println("Делитель равен 0");
-        }
+        int b = calculator.minus.apply(1, 1); // b == 0
+
+        int c = calculator.divide.apply(a, b);
+        calculator.println.accept(c);
     }
 }
